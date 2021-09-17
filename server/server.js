84d69b4a146400d7f6beb6dab289ff3089a1b5dd
J-Gilberg <85514341+J-Gilberg@users.noreply.github.com>
2021-09-17 16:30:41 -0500

    update

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('./server/config/mongoose.config');
+require('./config/mongoose.config');
 app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
-require('./server/routes/PirateCrew.routes')(app);
-require('./server/routes/User.routes')(app);
+require('./routes/PirateCrew.routes')(app);
+require('./routes/User.routes')(app);
  // this need to be changed per project
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
